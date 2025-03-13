095925bce297afb3c67108cf87c957f98c788be1
Binau02 <aubin.saunier@gmail.com>
2025-03-13 16:30:25 +0100

    ➕ add signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './feature/home/component/home/home.component';
 import { ListComponent } from './feature/list/component/list/list.component';
+import { SigninComponent } from './feature/signin/component/signin/signin.component';
+import { SignupComponent } from './feature/signup/component/signup/signup.component';
 
 export const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "list/:id", component: ListComponent},
+  {path: "signin", component: SigninComponent},
+  {path: "signup", component: SignupComponent},
 ];
--- src/app/feature/signin/component/signin/signin.component.ts
+++ src/app/feature/signin/component/signin/signin.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import { NavigationService } from '../../../../core/service/navigation.service';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule
+  ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  loginForm: FormGroup;
 
+  emailErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+
+  constructor(
+    private fb: FormBuilder,
+    protected navigationService : NavigationService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  updateEmailErrorMessage() {
+    if (this.loginForm.get("email")?.hasError('required')) {
+      this.emailErrorMessage.set('You must enter a value');
+    } else if (this.loginForm.get("email")?.hasError('email')) {
+      this.emailErrorMessage.set('Not a valid email');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+  }
+
+  updatePasswordErrorMessage() {
+    if (this.loginForm.get("password")?.hasError('required')) {
+      this.passwordErrorMessage.set('You must enter a value');
+    } else {
+      this.passwordErrorMessage.set('');
+    }
+  }
 }
