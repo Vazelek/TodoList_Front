cdb263def511099f072635c2dc686b5d467ec476
Vazelek <valentinherve60@gmail.com>
2025-03-13 17:23:10 +0100

    register

--- src/app/feature/signup/component/signup/signup.component.ts
+++ src/app/feature/signup/component/signup/signup.component.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {Component, inject, signal} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { NavigationService } from '../../../../core/service/navigation.service';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import {BACKEND_URI} from '../../../../core/component/constant/url.constant';
+import {User} from '../../../../core/type/user.type';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -17,18 +21,26 @@ import { NavigationService } from '../../../../core/service/navigation.service';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
-  standalone: true
+  standalone: true,
+  providers: [HttpClient]
 })
 export class SignupComponent {
-  loginForm: FormGroup;
+  public loginForm: FormGroup;
 
-  emailErrorMessage = signal('');
-  passwordErrorMessage = signal('');
-  passwordCheckErrorMessage = signal('');
+  public emailErrorMessage = signal('');
+
+  public passwordErrorMessage = signal('');
+
+  public passwordCheckErrorMessage = signal('');
+
+  private readonly http: HttpClient = inject(HttpClient);
+
+  private readonly router: Router = inject(Router);
 
   constructor(
     private fb: FormBuilder,
@@ -41,11 +53,24 @@ export class SignupComponent {
     });
   }
 
-  onSubmit() {
-
+  public onSubmit() {
+    const user: User = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    }
+    this.http.post(`${BACKEND_URI}/auth/register`, user, {withCredentials: true, responseType: 'json'}).subscribe({
+      next: (response: any) => {
+        console.log('Register next:', response);
+        this.router.navigate(['/signin']).then();
+      },
+      error: (error: any) => {
+        console.error('Register failed:', error);
+      }
+    }
+    );
   }
 
-  checkPasswordCheck() {
+  public checkPasswordCheck() {
     let password = this.loginForm.get("password")
     let passwordCheck = this.loginForm.get("passwordCheck")
 
@@ -57,7 +82,7 @@ export class SignupComponent {
     }
   }
 
-  updateEmailErrorMessage() {
+  public updateEmailErrorMessage() {
     if (this.loginForm.get("email")?.hasError('required')) {
       this.emailErrorMessage.set('You must enter a value');
     } else if (this.loginForm.get("email")?.hasError('email')) {
@@ -67,7 +92,7 @@ export class SignupComponent {
     }
   }
 
-  updatePasswordErrorMessage() {
+  public updatePasswordErrorMessage() {
     if (this.loginForm.get("password")?.hasError('required')) {
       this.passwordErrorMessage.set('You must enter a value');
     } else {
@@ -75,7 +100,7 @@ export class SignupComponent {
     }
   }
 
-  updatePasswordCheckErrorMessage() {
+  public updatePasswordCheckErrorMessage() {
     if (this.loginForm.get("passwordCheck")?.hasError('required')) {
       this.passwordCheckErrorMessage.set('You must enter a value');
     } else if (this.loginForm.get("passwordCheck")?.hasError('different')) {
