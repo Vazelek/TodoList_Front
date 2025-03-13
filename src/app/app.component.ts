32d0cb4704b279728a73bdca254523598bb86311
Vazelek <valentinherve60@gmail.com>
2025-03-13 09:31:32 +0100

    Main project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {NavbarComponent} from './core/component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'TodoList_Front';
-}
+export class AppComponent {}
