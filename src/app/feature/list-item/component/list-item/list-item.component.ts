24dc29fe2026d0e165e9cc4a3470b0889698e1da
Binau02 <aubin.saunier@gmail.com>
2025-03-13 17:27:15 +0100

    💄 add logo

--- src/app/feature/list-item/component/list-item/list-item.component.ts
+++ src/app/feature/list-item/component/list-item/list-item.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ListItem } from '../../../../core/type/list-item.type';
 import { NavigationService } from '../../../../core/service/navigation.service';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-list-item',
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.scss',
   standalone: true
