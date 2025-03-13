4cce8370c054f5226ab28c65129af01704876395
Vazelek <valentinherve60@gmail.com>
2025-03-13 17:28:16 +0100

    login

--- src/app/feature/signin/component/signin/signin.component.ts
+++ src/app/feature/signin/component/signin/signin.component.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {Component, inject, signal} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { NavigationService } from '../../../../core/service/navigation.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {BACKEND_URI} from '../../../../core/component/constant/url.constant';
+import {User} from '../../../../core/type/user.type';
 
 @Component({
   selector: 'app-signin',
@@ -17,17 +21,23 @@ import { NavigationService } from '../../../../core/service/navigation.service';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss',
-  standalone: true
+  standalone: true,
+  providers: [HttpClient]
 })
 export class SigninComponent {
   loginForm: FormGroup;
 
   emailErrorMessage = signal('');
   passwordErrorMessage = signal('');
+
+  private readonly http: HttpClient = inject(HttpClient);
+
+  private readonly router: Router = inject(Router);
 
   constructor(
     private fb: FormBuilder,
@@ -40,7 +50,23 @@ export class SigninComponent {
   }
 
   onSubmit() {
-
+    const user: User = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    }
+    this.http.post(`${BACKEND_URI}/auth/login`, user, {withCredentials: true, responseType: 'json'}).subscribe({
+      next: (response: any) => {
+        console.log('Login next:', response);
+        this.router.navigate(['']).then();
+      },
+      error: (error: any) => {
+        if (error.status === 401) {
+          console.error('Login failed: Forbidden', error);
+        } else {
+          console.error('Login failed:', error);
+        }
+      }
+    });
   }
 
   updateEmailErrorMessage() {
