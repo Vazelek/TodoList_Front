2f20dcb868341a7409dd027e1e265d1418b61575
Binau02 <aubin.saunier@gmail.com>
2025-03-13 15:44:58 +0100

    💄 style navbar

--- src/app/core/component/navbar/navbar.component.ts
+++ src/app/core/component/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavigationService } from '../../service/navigation.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; 
+
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   standalone: true,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor (protected navigationService : NavigationService) {}
+}
