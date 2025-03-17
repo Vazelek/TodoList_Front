4c2d8c0933d93917010736a3d87488be5208020c
Vazelek <valentinherve60@gmail.com>
2025-03-17 11:10:09 +0100

    auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { HomeComponent } from './feature/home/component/home/home.component';
 import { ListComponent } from './feature/list/component/list/list.component';
 import { SigninComponent } from './feature/signin/component/signin/signin.component';
 import { SignupComponent } from './feature/signup/component/signup/signup.component';
+import {AuthGuard} from './core/guard/auth.guard';
 
 export const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: "list/:id",
-    component: ListComponent
+    component: ListComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: "signin",
