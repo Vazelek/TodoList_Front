67938f41f82d6514ad2e6693b5911815c87f01cd
Vazelek <valentinherve60@gmail.com>
2025-03-17 15:12:15 +0100

    grant access to user

--- src/app/feature/list/component/list/list.component.ts
+++ src/app/feature/list/component/list/list.component.ts
@@ -1,4 +1,12 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
 import { TaskItem } from '../../../../core/type/task-item.type';
@@ -13,8 +21,12 @@ import {DateAdapter, provideNativeDateAdapter} from '@angular/material/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import {subscribe} from 'node:diagnostics_channel';
+import {MatList, MatListItem} from '@angular/material/list';
+import {User} from '../../../../core/type/user.type';
+import {sign} from 'node:crypto';
 
 interface List { name: string , id: number }
+interface GrantAccess { email: string , list_id: number }
 
 @Component({
   selector: 'app-list',
@@ -29,7 +41,9 @@ interface List { name: string , id: number }
     MatDatepickerInput,
     MatDatepickerToggle,
     ReactiveFormsModule,
-    MatButton
+    MatButton,
+    MatList,
+    MatListItem
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
@@ -43,15 +57,21 @@ export class ListComponent implements OnInit {
 
   public listItem : List | undefined = undefined;
 
+  public users: User[] = [];
+
   public newTaskFormGroup!: FormGroup;
+
+  public grantAccessFormGroup!: FormGroup;
 
   private nameFormControl: FormControl<string | null> = new FormControl(null, [Validators.required]);
 
   private dateFormControl: FormControl<Date | null> = new FormControl(null, [Validators.required]);
 
+  private emailFormControl: FormControl<string | null> = new FormControl(null, [Validators.required, Validators.email]);
+
   private readonly http: HttpClient = inject(HttpClient);
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cdr: ChangeDetectorRef) {}
 
   public ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -64,9 +84,17 @@ export class ListComponent implements OnInit {
       this.taskItems = tasks.sort((a, b) =>  a.position - b.position);
     })
 
+    this.http.get<User[]>(`${BACKEND_URI}/list/${this.id}/users`, { withCredentials: true}).subscribe((users: User[]) => {
+      this.users = users;
+    })
+
     this.newTaskFormGroup = new FormGroup({
       name: this.nameFormControl,
       endDate: this.dateFormControl,
+    })
+
+    this.grantAccessFormGroup = new FormGroup({
+      email: this.emailFormControl,
     })
   }
 
@@ -85,6 +113,19 @@ export class ListComponent implements OnInit {
       { withCredentials: true }
     ).subscribe((task: TaskItem) => {
       this.taskItems.push(task);
+    })
+  }
+
+  public onGrantAccessSubmit(): void {
+    this.http.post<GrantAccess>(
+      `${BACKEND_URI}/lists/grant_access`,
+      {
+        email: this.emailFormControl.value,
+        list_id: parseInt(this.id as string),
+      },
+      { withCredentials: true }
+    ).subscribe(() => {
+      this.users.push({email: this.emailFormControl.value as string, has_right: 0});
     })
   }
 }
