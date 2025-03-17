cb1f22be05f2dc39ad9416d84b24ac8a1ce5f3af
Vazelek <valentinherve60@gmail.com>
2025-03-17 09:58:06 +0100

    get items of list

--- src/app/feature/list/component/list/list.component.ts
+++ src/app/feature/list/component/list/list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
 import { TaskItem } from '../../../../core/type/task-item.type';
 import { TaskItemComponent } from '../../../task-item/component/task-item/task-item.component';
+import {HttpClient} from '@angular/common/http';
+import {BACKEND_URI} from '../../../../core/component/constant/url.constant';
+import {ListItem} from '../../../../core/type/list-item.type';
 
+interface List { name: string , id: number }
 
 @Component({
   selector: 'app-list',
@@ -16,33 +20,30 @@ import { TaskItemComponent } from '../../../task-item/component/task-item/task-i
   styleUrl: './list.component.scss',
   standalone: true
 })
-export class ListComponent {
-  id: string | null = null;
+export class ListComponent implements OnInit {
+  public id: string | null = null;
+
+  public taskItems : TaskItem[] = [];
+
+  public listItem : List | undefined = undefined;
+
+  private readonly http: HttpClient = inject(HttpClient);
 
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+
+    this.http.get<List>(`${BACKEND_URI}/list/${this.id}`, { withCredentials: true}).subscribe((list: List) => {
+      this.listItem = list;
+    })
+
+    this.http.get<TaskItem[]>(`${BACKEND_URI}/list/${this.id}/items`, { withCredentials: true}).subscribe((tasks: TaskItem[]) => {
+      this.taskItems = tasks;
+    })
   }
 
-  tasks : TaskItem[] = [
-    {
-      id: 1,
-      name: "tache 1",
-      end_date: new Date(),
-      index: 0,
-      completed_by: null,
-    },
-    {
-      id: 2,
-      name: "tache 2",
-      end_date: new Date(),
-      index: 0,
-      completed_by: "ta mère",
-    },
-  ];
-
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.taskItems, event.previousIndex, event.currentIndex);
   }
 }
